d13f66caa334cde58d990749db08142da2e97a1f
wlsdzyzl <wlsdzyzl@gmail.com>
2020-09-24 00:36:36 +0800

    Fix bugs in example ReadPLYMesh(previously, it change bgr to rgb)

--- example/ReadPLYMesh.cpp
+++ example/ReadPLYMesh.cpp
@@ -10,21 +10,11 @@ int main(int argc, char* argv[])
         return 0;
     }
     geometry::TriangleMesh mesh;
-
     mesh.LoadFromPLY(argv[1]);
-    /*
-    if(!mesh.HasNormals())
-    {
-        std::cout << "Mesh don't have normals, estimate normals..."<<std::endl;
-        mesh.ComputeNormals();
-    }*/
-    for(int i = 0; i != mesh.colors.size(); ++i)
-    {
-        float tmp = mesh.colors[i](0);
-        mesh.colors[i](0) = mesh.colors[i](2);
-        mesh.colors[i](2) = tmp;
-    }
+
+
     visualization::Visualizer visualizer;
+    visualizer.SetDrawColor(true);
     visualizer.AddTriangleMesh(mesh);
     visualizer.Show();
     mesh.WriteToPLY("./open3d.ply");
